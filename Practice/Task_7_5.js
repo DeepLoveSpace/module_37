285f91ed93c1b946cf1b4e423aead55055d12b72
DeepLove <titan182@yandex.ru>
2026-09-16 17:15:12 +0300

    Практика модуля 38

---
+++ Practice/Task_7_5.js
@@ -0,0 +1,12 @@
+const numbers = [12, 3, 7, 4, 9, 15, 1, 8, 2, 6, 19, 3, 1, 12, 3, 8, 6];
+
+function uniqueSorted(numbers) {
+    const uniqueNumbers = [...new Set(numbers)];
+    const sortedNumbers = uniqueNumbers.sort((a, b) => a - b);
+    return sortedNumbers;
+}
+
+const result = uniqueSorted(numbers);
+
+console.log(result);
+console.log(numbers);
