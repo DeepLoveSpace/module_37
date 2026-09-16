c47ccbaf91c4f87981d8ef9b2fc18c79b455d2b2
DeepLove <titan182@yandex.ru>
2026-09-16 17:47:50 +0300

    Удалил старые задания

--- Task/module_37/Task_6_2.js
+++
@@ -1,6 +0,0 @@
-// Задание 6.2
-
-const arr = [1, 2, 3, 1, 5, 4, 2, 3, 5, 'they', 'don\'t', 'know', 'that', 'we', 'know', 'that', 'they', 'know'];
-const uniqueSet = new Set(arr);
-const uniqueArr = Array.from(uniqueSet);
-console.log(uniqueArr);
