401f710ac3105057eb1b07af26e9a7a17f4c331c
DeepLove <titan182@yandex.ru>
2026-09-17 14:36:50 +0300

    Поправил названия папок и файлов

---
+++ Practice/task-7-4.js
@@ -0,0 +1,15 @@
+const person = {
+    firstName: "john",
+    lastName: "Doe"
+};
+
+function setFullName(fullName) {
+    this.fullName = fullName;
+}
+
+const setPersonFullName = setFullName.bind(person);
+
+setPersonFullName("John Smith");
+
+console.log(person.fullName);
+console.log(person);
